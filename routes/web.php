00ba27105cb86447ec86c7ae4da12c25b284d4c3
Harun Doğdu <Harundogdu06@gmail.com>
2021-08-06 23:52:08 +0300

    Tema parçalama işlemleri yapıldı

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\FrontController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -13,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/',[FrontController::class,'index'])->name('index');
+Route::get('/resume',[FrontController::class,'resume'])->name('resume');
+Route::get('/portfolio',[FrontController::class,'portfolio'])->name('portfolio');
+Route::get('/blog',[FrontController::class,'blog'])->name('blog');
+Route::get('/contact',[FrontController::class,'contact'])->name('contact');
